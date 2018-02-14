589546711da1222048a322fdfb9cc3e78009f607
Pavel Margolin <pavel@jet.com>
2018-02-14 08:53:19 -0500

    solved problem3 with binSearch algorithm...needs some cleanup

--- problem2/Program.fs
+++ problem2/Program.fs
@@ -24,91 +24,108 @@ let parse (lines : string []) =
     |> Array.choose split
     |> Array.map Sku.create
 
+let pause () =
+    Console.ReadLine() |> ignore
 
-let binarySearch maxPrice (skus : Sku []) = 
-    printfn "started in binary search total=%d" skus.Length
-    let rec loop acc left right  = 
-        printfn "entered acc=%A left=%d right=%d" acc left right
-        if right = left then 
-            if skus.[left].price > acc.price then 
-                skus.[left]
-            else 
-                acc
-        elif right < left then
+// Using binary search O(log n) finds the sku with the 
+// highest price less than the maximum specified
+// Assumes all the sku's are in ascending order by price
+let binSearch max (sku : Sku []) = 
+    let skuMaxPrice (left:Sku) (right:Sku) =
+        // find the max of two Sku's if they are equal return the first one
+        if left.price >= right.price then
+            left
+        else 
+            right
+
+    let rec loop acc left right = 
+        // printfn "Entered loop acc=%O left=%d right=%d" acc left right
+        // pause()
+        if left > right then 
             acc
-        else
-            let midpoint = (right-left + 1)/2 + left
-            printfn "entered loop acc=%A left=%d right=%d midpoint=%d" acc left right midpoint
-            let sku = skus.[midpoint]
-            if sku.price = maxPrice then 
-                printfn "found max price sku=%A" sku
-                sku 
-            elif sku.price > maxPrice then
-                printfn "its greater than max, going left"
-                loop acc left (midpoint - 1)
-            elif sku.price > acc.price then
-                printfn "Found greater accumulator than max, going right"
-                loop sku (midpoint + 1) right
+        elif left = right then
+            let midpoint = sku.[left]
+            if midpoint.price <= max then
+                skuMaxPrice acc midpoint
             else
-                printfn "Going right"
-                loop acc (midpoint+1) right
-    loop skus.[0] 0 (skus.Length-1)
-
-let findPairing maxItems balance (skus : Sku []) = 
-    let rec loop acc depth maxBalance (xs:Sku list) = 
-        if depth = 0 then
-            acc
+                acc
         else
-            match xs with
-            | [] -> acc
-            | x::xs -> 
-                let remainingBalance = maxBalance - x.price
-                printfn "remaining balance=%d price=%d"  remainingBalance x.price
-                let best = binarySearch remainingBalance (xs |> List.toArray)
-                printfn "found best=%A" best
-                loop (best::acc) (depth-1) remainingBalance xs
-    loop [] maxItems balance (skus |> List.ofArray)
-        
-let findLargest balance (skus : Sku []) =
+            let midpoint = (right - left)/2 + left 
+            let item = sku.[midpoint]
+            if item.price = max then 
+                item
+            elif item.price < max then 
+                // go right
+                let acc = skuMaxPrice acc item
+                loop acc (midpoint+1) right
+            else
+                // go left
+                loop acc left (midpoint-1)
+    if sku.Length = 0 || sku.[0].price > max then 
+        None
+    else
+        loop sku.[0] 0 (sku.Length-1) |> Some
+
+
+let findLargest (search : int -> Sku[] -> Sku option)  balance (skus : Sku []) =
     let rec loop acc rem = function
         | [] -> acc
         | x::xs -> 
             let nextRem = rem - x.price 
             printfn "nextRem=%d currentItem=%O" nextRem x
+            pause()
             if nextRem > 0 then 
                 //take the first element, find the largest next element
-                let filtered = xs |> List.filter (fun x -> x.price <= nextRem) 
-                if Seq.isEmpty filtered then 
-                    printfn "Nothing less than nextRem=%d" nextRem
-                    loop acc balance xs
-                else
-                    let largest = filtered |> List.maxBy (fun x -> x.price)
-                    printfn "found largest=%O" largest
-                    loop ((x,largest) :: acc) nextRem xs 
+                match search nextRem (xs |> List.toArray) with
+                | None -> loop acc balance xs
+                | Some largest -> loop ((x,largest) :: acc) balance xs 
             else 
                 printfn "nextRem=0"
                 loop (acc) balance xs
+
+    let createPairs arr = 
+        let rec loop acc = function 
+            | [] -> acc |> List.rev
+            | x::xs -> loop ((x,xs)::acc) xs
+        loop [] (arr |> Array.toList)
+
+
+    let rec sequence depth maxPrice (xs:Sku[]) = 
+        match depth with 
+        | 0 | _ when xs.Length = 0 -> Seq.empty
+        | 1 -> 
+            match search maxPrice xs with
+            | Some x -> [x] |> Seq.singleton
+            | None -> Seq.empty
+        | d  -> 
+            seq {
+                for (head,tail) in createPairs xs do
+                    let nextMax = maxPrice - head.price
+                    if nextMax <= 0 then 
+                        yield! Seq.empty
+                    else 
+                        printfn "Depth=%d looking for matches for %O with remaining balance=%d" d head nextMax
+                        let results = sequence (d-1) nextMax (tail |> List.toArray)
+                        printfn "found %d results" (Seq.length results)
+                        if Seq.isEmpty results then
+                            yield! Seq.empty
+                        else 
+                            for r in results do 
+                                yield head :: r
+            }
+
     loop [] balance (skus |> Array.toList)
 
-let test skus = 
-    let go num = findLargest num skus |> Seq.iter (fun (x,y) -> printfn "(%s,%d)-(%s,%d)" x.name x.price y.name y.price)
-    go 2000
+
+let go num skus = 
+    let search = binSearch
+    findLargest search num skus |> Seq.iter (fun (x,y) -> printfn "(%s,%d)-(%s,%d)" x.name x.price y.name y.price)
 
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
-
-    let testSegment = 
-        let arr = [|0..9|]
-        let seg = new ArraySegment<_>(arr)
-        let ilist : IList<_> = upcast seg
-        ilist.RemoveAt(5)
-        let res = ilist |> Seq.toArray
-        printfn "res=%A\nseq=%A\narr=%A" res seg arr
-
-    Environment.Exit(0)
     let filename = Path.Combine("problem2","prices.txt")
-    let filename = argv.[0]
+    // let filename = argv.[0]
     let balance = Int32.Parse argv.[1] 
     if not <| File.Exists filename then
         printfn "File %s does not exist" filename
@@ -116,6 +133,6 @@ let main argv =
         let file = File.ReadAllLines(filename)
         let skus = file |> parse
         
-        binarySearch 800 skus
+        go 2000 skus
         ()
     0 // return an integer exit code
