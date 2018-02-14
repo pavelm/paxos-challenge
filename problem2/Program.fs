0c39e166f884f3aaea83438d16472215a63ca2d3
Pavel Margolin <pavel@jet.com>
2018-02-14 14:29:27 -0500

    cleanup problem2

--- problem2/Program.fs
+++ problem2/Program.fs
@@ -2,7 +2,6 @@
 
 open System
 open System.IO
-open System.Collections.Generic
 
 
 type Sku = {
@@ -11,7 +10,6 @@ type Sku = {
 } with 
     override x.ToString() = sprintf "(%s,%d)" x.name x.price
     static member create(name,price) = { name = name; price = Int32.Parse price}
-
 
 
 let parse (lines : string []) =
@@ -23,9 +21,6 @@ let parse (lines : string []) =
     lines
     |> Array.choose split
     |> Array.map Sku.create
-
-let pause () =
-    Console.ReadLine() |> ignore
 
 // Using binary search O(log n) finds the sku with the 
 // highest price less than the maximum specified
@@ -39,8 +34,6 @@ let binSearch max (sku : Sku []) =
             right
 
     let rec loop acc left right = 
-        // printfn "Entered loop acc=%O left=%d right=%d" acc left right
-        // pause()
         if left > right then 
             acc
         elif left = right then
@@ -67,28 +60,17 @@ let binSearch max (sku : Sku []) =
         loop sku.[0] 0 (sku.Length-1) |> Some
 
 
-let findLargest (search : int -> Sku[] -> Sku option)  balance (skus : Sku []) =
-    let rec loop acc rem = function
-        | [] -> acc
-        | x::xs -> 
-            let nextRem = rem - x.price 
-            printfn "nextRem=%d currentItem=%O" nextRem x
-            pause()
-            if nextRem > 0 then 
-                //take the first element, find the largest next element
-                match search nextRem (xs |> List.toArray) with
-                | None -> loop acc balance xs
-                | Some largest -> loop ((x,largest) :: acc) balance xs 
-            else 
-                printfn "nextRem=0"
-                loop (acc) balance xs
-
-    let createPairs arr = 
-        let rec loop acc = function 
-            | [] -> acc |> List.rev
-            | x::xs -> loop ((x,xs)::acc) xs
-        loop [] (arr |> Array.toList)
-
+let findLargest (search : int -> Sku[] -> Sku option) numSkus  balance (skus : Sku []) =
+    let rec pairGen arr =
+        seq {
+            match arr with 
+            | [||] -> yield! Seq.empty
+            | xs -> 
+                let head = Array.head xs
+                let tail = Array.tail xs
+                yield (head,tail)
+                yield! pairGen tail 
+        }
 
     let rec sequence depth maxPrice (xs:Sku[]) = 
         match depth with 
@@ -99,33 +81,23 @@ let findLargest (search : int -> Sku[] -> Sku option)  balance (skus : Sku []) =
             | None -> Seq.empty
         | d  -> 
             seq {
-                for (head,tail) in createPairs xs do
+                for (head,tail) in pairGen xs do
                     let nextMax = maxPrice - head.price
                     if nextMax <= 0 then 
                         yield! Seq.empty
                     else 
-                        printfn "Depth=%d looking for matches for %O with remaining balance=%d" d head nextMax
-                        let results = sequence (d-1) nextMax (tail |> List.toArray)
-                        printfn "found %d results" (Seq.length results)
+                        let results = sequence (d-1) nextMax (tail)
                         if Seq.isEmpty results then
                             yield! Seq.empty
                         else 
                             for r in results do 
                                 yield head :: r
             }
-
-    loop [] balance (skus |> Array.toList)
-
-
-let go num skus = 
-    let search = binSearch
-    findLargest search num skus |> Seq.iter (fun (x,y) -> printfn "(%s,%d)-(%s,%d)" x.name x.price y.name y.price)
+    sequence numSkus balance skus
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    let filename = Path.Combine("problem2","prices.txt")
-    // let filename = argv.[0]
+    let filename = argv.[0]
     let balance = Int32.Parse argv.[1] 
     if not <| File.Exists filename then
         printfn "File %s does not exist" filename
@@ -133,6 +105,19 @@ let main argv =
         let file = File.ReadAllLines(filename)
         let skus = file |> parse
         
-        go 2000 skus
-        ()
+        let badSearch max skus = 
+            let filtered = skus |> Array.filter (fun sku -> sku.price <= max) 
+            if Array.isEmpty filtered then
+                None
+            else
+                filtered |> Array.maxBy (fun sku -> sku.price ) |> Some
+
+        let searchAlgo = binSearch
+        let sw = System.Diagnostics.Stopwatch()
+        sw.Start()
+        let results = findLargest searchAlgo 2 balance skus |> Seq.toArray //iterate through all
+        sw.Stop()
+        results |> Seq.length |> printfn "SOLUTION results %O"
+        printfn "Total elapsed time=%O ticks=%d" sw.ElapsedMilliseconds sw.ElapsedTicks
+
     0 // return an integer exit code
